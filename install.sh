f88072e848fdc266172e170a89bc28053487d883
Daniel Nesbitt <dan.nesbitt@gmail.com>
2013-08-11 20:55:00 -0400

    Fix directory linking.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Find out where we're running from
-DIR=`dirname $0`
+DIR=`dirname $0:A`
 
 # Install zsh
 ln -s $DIR/zsh/oh-my-zsh $HOME/.oh-my-zsh
@@ -9,5 +9,5 @@ ln -s $DIR/zsh/zshrc $HOME/.zshrc
 
 # Install Sublime 2 user settings
 if [[ $OSTYPE == darwin* ]]; then
-	ln -s $DIR/sublime2 $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+	ln -s $DIR/sublime2 "$HOME/Library/Application Support/Sublime Text 2/Packages/User"
 fi
